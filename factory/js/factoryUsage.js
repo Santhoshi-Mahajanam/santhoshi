0db1c7ddec0ca25d97d1719100888be2ff64a711
Santhoshi-Mahajanam <mahajanamratnasanthoshi@gmail.com>
2016-12-09 17:58:37 +0530

    uploaded usage of $q.all

--- factory/js/factoryUsage.js
+++ factory/js/factoryUsage.js
@@ -8,7 +8,23 @@ app.controller("factoryCtrl",function($scope,factoryLessPromise){
 	function(input){
 		console.log("sorry");
 	});
+});
+
+app.controller("loopServiceCtrl",function($scope,loopPromiseService,$q){
+
+	var loop1;
+	var loop2;
+	loop1=loopPromiseService.promise1("js/serviceData.json");
+	
+	loop2=loopPromiseService.promise1("js/factoryData.json");
+	
+	$q.all([loop1,loop2]).then(function(response){
+		$scope.data1=response[0];
+		console.log($scope.data1)
+		$scope.data2=response[1];
 	});
+
+});
 
 app.factory("factoryLessPromise",function($http,$q){
 	var factory={};
@@ -17,7 +33,6 @@ app.factory("factoryLessPromise",function($http,$q){
 		return $http.get("js/factoryData.json")
 		.then(function(response){
 			deferred.resolve(response.data.cartoons);
-			console.log(response.data.cartoons);
 			return deferred.promise;
 		},
 		function(response){
@@ -29,3 +44,18 @@ app.factory("factoryLessPromise",function($http,$q){
 	return factory;
 	
 });
+
+app.service("loopPromiseService",function($http,$q){
+	var defer1=$q.defer();
+	this.promise1=function(arrayLoop){
+		return $http.get(arrayLoop)
+		.then(function(response){
+			defer1.resolve(response.data);
+			console.log(response.data);
+			return defer1.promise;
+		},function(response){
+			defer1.reject(data);
+			return defer1.promise;
+		});
+	};
+})
